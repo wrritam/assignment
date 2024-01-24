a17c8afc31cf840a7cb8e8d087eaf40ecf2a411d
Ritam Samanta <bruceritamwayne02@gmail.com>
2024-01-24 06:57:37 +0530

    few changes made

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const port = 9000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Backend is live.");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.use("/api", dataFetch);
